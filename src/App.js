70d4dcc7b5b6c13035bb3b27cff57334dfae813a
himanshupowerweave <139436979+himanshupowerweave@users.noreply.github.com>
2023-08-01 12:00:12 +0530

    Upload assignment 2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Header from "../src/components/Header.jsx";
+import Form from "../src/components/Form.jsx";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import logo from '../logo.png';
+const Header = () => {
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+        </header>
+    );
+};
+export default Header;
